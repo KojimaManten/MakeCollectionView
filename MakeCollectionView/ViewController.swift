7b1a81740a1cda4c2fc1db974ef9167a42736ba8
KojimaManten <jumper.manten@icloud.com>
2020-07-12 21:01:57 +0900

    UICollectionFlowLayoutを実装してレイアウトを設定した

--- MakeCollectionView/ViewController.swift
+++ MakeCollectionView/ViewController.swift
@@ -22,17 +22,44 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    //1セクションに入るセルの数
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 20
     }
-    
+    //セクションの数
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+       return 2
+    }
+    //セルの中身
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
         cell.backgroundColor = .red
         return cell
     }
-    
-    
 }
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    //collectionViewの大きさ変更可能
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if indexPath.section == 0 {
+            return CGSize(width: 50, height: 50)
+        } else {
+            return CGSize(width: 100, height: 100)
+        }
+    }
+    //画面４方向からの長さ
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 100, left: 0, bottom: 0, right: 0)
+    }
+    //セル同士の縦の間隔
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return CGFloat(100)
+    }
+    //セル同士の横の間隔
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return CGFloat(20)
+    }
+}
+
 
 
